1e29aa3bc8041ad19bad0a80cb06aec9db9289e2
Dr. Cat <47220664+catsmells@users.noreply.github.com>
2025-04-07 21:01:13 -0400

    Create Setup.php

---
+++ src/addons/BeerBuddy/Setup.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace BeerBuddy;
+
+use XF\AddOn\AbstractSetup;
+
+class Setup extends AbstractSetup
+{
+    public function install(array $stepParams = [])
+    {
+        // database placeholder
+    }
+
+    public function uninstall(array $stepParams = [])
+    {
+       // uninstall script here later
+    }
+
+    public function upgrade(array $stepParams = [])
+    {
+        // upgrade shit here
+    }
+}
